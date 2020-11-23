d85a4aace64eadbda2c83848eec00854ee9ba0b1
sealfur <jkinal@thoughtworks.com>
2020-11-23 14:35:35 +1100

    added nunjucks templating

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,11 @@
 
 module.exports = (config) => {
   return {
+    markdownTemplateEngine: 'njk', //using nunjucks as templating engine
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist',
     },
   };
-};
