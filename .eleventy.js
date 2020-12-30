c8975c76603504996c02dc303bed641933dae0be
sealfur <jkinal@thoughtworks.com>
2020-12-30 12:38:59 +1100

    fixed the doubling up on  elements

--- .eleventy.js
+++ .eleventy.js
@@ -13,9 +13,21 @@ const markdownLib = markdownIt(options).use(markdownItFootnote);
 
 module.exports = (config) => {
   // ----Set directories to pass through to the dist folder
-  config.addPassthroughCopy('.src/images/')
+  config.addPassthroughCopy('.src/images/');
+
   // ------Markdown overrides----
   config.setLibrary('md', markdownLib);
+
+  //---Returns all clippings sorted by "rating"
+  //   which will be in the front-matter of all clippings.
+  //   They will probably flow by 
+  config.addCollection('clippings', (collection) => {
+    return collection
+      .getFilteredByGlob('./src/clippings/*.md')
+      .sort((a, b) =>
+        Number(a.data.rating) > Number(b.data.rating) ? 1 : -1
+      );
+  });
 
   return {
     markdownTemplateEngine: 'njk', //using nunjucks as templating engine
