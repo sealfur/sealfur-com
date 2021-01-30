c5a581c131f486a2f26e2ba5c418dd42fe1f5220
sealfur <jkinal@thoughtworks.com>
2021-01-30 18:23:42 +1100

    added fonts from google to the site

---
+++ gulp-tasks/fonts.js
@@ -0,0 +1,21 @@
+// see [eleventy from scratch lesson about pulling fonts in](https://piccalil.li/course/learn-eleventy-from-scratch/lesson/20/)
+const { dest, src } = require('gulp');
+const GetGoogleFonts = require('get-google-fonts');
+
+const fonts = async () => {
+  // Setup of the library instance by setting where we want
+  // the output to go. CSS is relative to output font directory
+  const instance = new GetGoogleFonts({
+    outputDir: './dist/fonts',
+    cssFile: './fonts.css',
+  });
+
+  // Grabs fonts and CSS from google and puts in the dist folder
+  const result = await instance.download(
+    'https://fonts.googleapis.com/css2?family=Inter:wght@335;525;690&family=Sansita+Swashed:wght@571;636&display=swap'
+  );
+
+  return result;
+};
+
+module.exports = fonts;
