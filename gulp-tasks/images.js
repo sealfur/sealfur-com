18b25bc238ee23426194aec6ccabb19d0a0dc5bf
Joshua Kinal <jkinal@thoughtworks.com>
2021-02-12 15:48:16 +1100

    fixed issue 4 with images not rendering and favicon sizing

---
+++ gulp-tasks/images.js
@@ -0,0 +1,25 @@
+const {dest, src} = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+// Grabs all images, runs them through `imagemin`
+// then plops them in the dist folder.
+const images = () => {
+
+  // We have specific configs for jpeg and png files
+  // to try to really bring down asset sizes
+  return src('./src/images/**/*')
+  .pipe(
+    imagemin(
+      [
+        imagemin.mozjpeg({quality: 60, progressive: true}),
+        imagemin.optipng({optimizationLevel: 5, interlaced: null})
+      ],
+      {
+        silent: true
+      }
+    )
+  )
+  .pipe(dest('./dist/images'));
+};
+
+module.exports = images;
